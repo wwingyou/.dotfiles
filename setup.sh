5a85be3aa728f7e9a5d5a07cc20c5403d149f2ab
wingyou <winwing.you@gmail.com>
2024-09-24 22:43:20 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+DIR="$(cd "$(dirname "$0")" && pwd)"
+ln -s $DIR/nvim ~/.config/nvim
+ln -s $DIR/kitty ~/.config/kitty
+ln -s $DIR/karabiner ~/.config/karabiner
+ln -s $DIR/nnn ~/.config/nnn
+ln -s $DIR/tmux ~/.tmux
+ln -s $DIR/tmux.conf ~/.tmux.conf
+ln -s $DIR/tmuxifier ~/.tmuxifier
+ln -s $DIR/zshrc ~/.zshrc
