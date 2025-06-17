5f23e8abdee8d881fc5421662703047013d43807
wingyou <winwing.you@gmail.com>
2025-06-17 21:24:08 +0900

    seperate zshrc custom settings

---
+++ custom_zshrc.sh
@@ -0,0 +1,36 @@
+# NOTE: 
+# >> THIS PART MIGHT NOT WORK
+ZSH_THEME="spaceship"
+ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+source $ZSH/oh-my-zsh.sh
+# << THIS PART MIGHT NOT WORK
+
+prompt_context() {
+  prompt_segment black default "👾"
+}
+
+# alias
+alias vim=nvim
+alias lg=lazygit
+alias dls="ll | grep ^d"
+
+# fzf setting
+if type rg &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='rg --files'
+  export FZF_DEFAULT_OPTS='-m'
+fi
+
+# fzf shell integration
+source <(fzf --zsh)
+
+# Set default editor to nvim
+export EDITOR=nvim
+
+# Quick test alias for Spring Boot application testing and debuging
+ts() {
+  ./gradlew test
+  if [ $? -ne 0 ]; then
+    open build/reports/tests/test/index.html
+  fi
+}
--- setup.sh
+++ setup.sh
@@ -6,9 +6,12 @@ ln -s $DIR/nnn ~/.config/nnn
 ln -s $DIR/tmux ~/.tmux
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/tmuxifier ~/.tmuxifier
-ln -s $DIR/zshrc ~/.zshrc
+ln -s $DIR/custom_zshrc.sh ~/.custom_zshrc.sh
 
 # Install tmux tokyonight dependencies
 # See: https://github.com/janoamaral/tokyo-night-tmux?tab=readme-ov-file#requirements
 brew install --cask font-monaspace-nerd-font font-noto-sans-symbols-2
 brew install bash bc coreutils gawk gh glab gsed jq nowplaying-cli
+
+# Append zshrc custom setting load script
+echo "\n# zshrc custom settings\nsource .custom_zshrc.sh" >> "${DIR}/.zshrc"
