046a07a578749979e93e7f25659e3387574f2453
wingyou <winwing.you@gmail.com>
2025-06-22 16:53:09 +0900

    FIX(#6): Correct custom-zshrc.sh loading script injection location

--- setup-macos.sh
+++ setup-macos.sh
@@ -6,5 +6,5 @@ ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/custom_zshrc.sh ~/.custom_zshrc.sh
 
 # Append zshrc custom setting load script
-echo "\n# zshrc custom settings\nsource .custom_zshrc.sh" >> "${DIR}/.zshrc"
+echo "\n# zshrc custom settings\nsource .custom_zshrc.sh" >> "~/.zshrc"
 
